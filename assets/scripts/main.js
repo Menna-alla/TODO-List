233f174ea23ac8f731dd95fcd33c688bc43f2de8
menna samy <mmeennaa620@gmail.com>
2025-01-15 09:28:57 +0200

    Add: addTask & showTask & Storage Data in LocalStorage & show Data From Local Storage

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,3 +5,26 @@ document.getElementById("toggle-theme").onclick = () => {
     document.body.classList.add("dark-theme");
   }
 };
+
+let taskList = "";
+if (localStorage.getItem("taskes") !== null) {
+  taskList = localStorage.getItem("tasks");
+}
+
+function addTask() {
+  let taskInput = document.getElementById("task-input");
+  let taskStatus = "In Progress";
+  taskList += `<li>${taskInput.value}<br>${new Date().toLocaleDateString()}
+  `;
+  localStorage.setItem("tasks", taskList);
+taskInput.value = "";
+showTask();
+
+}
+
+function showTask() {
+  document.getElementById("task-list").innerHTML = localStorage.getItem("tasks");
+}
+showTask();
+
+document.getElementById("add-task-btn").onclick = () => addTask();
